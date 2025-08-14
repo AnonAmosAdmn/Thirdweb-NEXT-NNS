d6ba7e7c3eb77c572f30d4971cf532ec42ba11a1
AnonAmosAdmn <122024790+AnonAmosAdmn@users.noreply.github.com>
2025-08-14 10:38:39 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["pino-pretty"],
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "ipfs.io" },
+      { protocol: "https", hostname: "gateway.pinata.cloud" },
+      { protocol: "https", hostname: "nftstorage.link" },
+      { protocol: "https", hostname: "pbs.twimg.com" } // ✅ Twitter avatars
+    ],
+  },
 };
 
 export default nextConfig;
+
